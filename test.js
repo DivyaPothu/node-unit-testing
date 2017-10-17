7e7c93375ba8fcb3d67e11b9b34f55400aa82aaf
Divya Pothu <divya.pothu@tcs.com>
2017-10-17 16:24:36 +0530

    mocha array example

--- test.js
+++ test.js
@@ -18,3 +18,14 @@ it('should return false when num is odd',function(){
 expect(isEven(5)).to.be.false;
 })
 });
+
+
+
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
