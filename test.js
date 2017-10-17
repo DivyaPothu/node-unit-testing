aac3a377b5add53abeebef58a7b28f0f3b9f3280
Divya Pothu <divya.pothu@tcs.com>
2017-10-17 16:09:11 +0530

    mocha even odd example

---
+++ test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var chai =require('chai'),
+expect=chai.expect;
+
+chai.should();
+
+function isEven(num){
+return num%2 ==0;
+}
+
+describe('isEven',function(){
+it('should return true when number is even',function(){
+isEven(4).should.be.true;
+});
+
+it('should return false when num is odd',function(){
+expect(isEven(5)).to.be.false;
+})
+});
